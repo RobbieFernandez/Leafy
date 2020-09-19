cb0d529ad428c01b5d1b7ab8d27455ce75046d21
Robert Fernandez <fernandez.robert.97@gmail.com>
2020-09-19 22:55:25 +0800

    Initial commit

---
+++ leafy/plants/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+
+class Plant(models.Model):
+    class DayOfTheWeek(models.TextChoices):
+        MONDAY = ('MONDAY', 'Monday')
+        TUESDAY = ('TUESDAY', 'Tuesday')
+        WEDNESDAY = ('WEDNESDAY', 'Wednesday')
+        THURSDAY = ('THURSDAY', 'Thursday')
+        FRIDAY = ('FRIDAY', 'Friday')
+        SATURDAY = ('SATURDAY', 'Saturday')
+        SUNDAY = ('SUNDAY', 'Sunday')
+
+    name = models.CharField(max_length=512)
+
+    watering_day = models.PositiveIntegerField(
+        choices=DayOfTheWeekchoices
+    )
+
+    def __str__(self):
+        return self.name
