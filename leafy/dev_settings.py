5f6d12e6b169a735928ca243c7ca80983420aaaa
Robert Fernandez <fernandez.robert.97@gmail.com>
2021-03-13 14:42:13 +0800

    Get settings.SECRET_KEY from environment variable

--- leafy/dev_settings.py
+++ leafy/dev_settings.py
@@ -9,3 +9,4 @@ ALLOWED_HOSTS = ["*"]
 
 DEBUG = False
 
+SECRET_KEY = '&=9mzx%benre*_gl+1#sf_m7uc6@c#4r5y))g9232io$ulgipk'
--- leafy/settings.py
+++ leafy/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&=9mzx%benre*_gl+1#sf_m7uc6@c#4r5y))g9232io$ulgipk'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
